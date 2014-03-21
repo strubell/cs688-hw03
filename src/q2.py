9731f22b0842a5d442745209c72b3558f38b48b4
Emma Strubell <strubell@cs.umass.edu>
2014-03-21 15:00:38 -0400

    q2a done

--- src/q2.py
+++ src/q2.py
@@ -12,8 +12,8 @@ from matplotlib import cm
 from numpy.random import rand
 import denoise
 
-orig_fname = "../data/stripes.txt"
-noise_fname = "../data/stripes-noise.txt"
+orig_fname = "../data/swirl.txt"
+noise_fname = "../data/swirl-noise.txt"
 
 orig_data = np.loadtxt(orig_fname)
 noise_data = np.loadtxt(noise_fname)
@@ -21,36 +21,26 @@ noise_data = np.loadtxt(noise_fname)
 do_plot = True
 
 # initialize w_p, w_l to (single) positive value each
+w_l = 100.0
+w_p = 100.0
 
-# over-smoothed
-#w_l = 0.0001
-#w_p = 0.9
-
-# under-smoothed
-#w_l = 0.5
-#w_p = 0.5
-
-# good
-w_l = 1.0
-w_p = 20.0
-
-y, maes = denoise.denoise_binary(w_l, w_p, noise_data, orig_data, run_until_convergence=False)
+y, maes = denoise.denoise_greyscale(w_l, w_p, noise_data, orig_data, run_until_convergence=False)
 print "Final MAE:", maes[-1]
 
 if(do_plot):
-#     fig1 = plt.figure()
-#     ax1 = fig1.add_subplot(111)
-#     ax1.set_title("Original data (binary)")
-#     ax1.imshow(orig_data, interpolation='nearest', cmap='Greys')
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    ax1.set_title("Original data (greyscale)")
+    ax1.imshow(orig_data, interpolation='nearest', cmap='Greys')
       
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
-    ax2.set_title("Noisy data (binary)")
+    ax2.set_title("Noisy data (greyscale)")
     ax2.imshow(noise_data, interpolation='nearest', cmap='Greys')
     
     fig3 = plt.figure()
     ax3 = fig3.add_subplot(111)
-    ax3.set_title("Denoised data (binary) (MAE=%g)" % (maes[-1]))
+    ax3.set_title("Denoised data (greyscale) (MAE=%g)" % (maes[-1]))
     ax3.imshow(y, interpolation='nearest', cmap='Greys')
     
     fig4 = plt.figure()
